8ccfe3aa48754a50c7fddf41ead70c0ea0b74851
Karl <karl.wnw@gmail.com>
2018-11-27 19:07:13 +0100

    Fix urls compat for Django 1.10
    django.conf.urls.patterns() is Removed
    See https://docs.djangoproject.com/en/2.1/releases/1.10/#features-removed-in-1-10

--- provider/oauth2/urls.py
+++ provider/oauth2/urls.py
@@ -35,11 +35,12 @@ that are meant for client (as defined in :rfc:`1`) interaction.
 
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from ..compat.urls import *
+from django.conf.urls import url, include
+
 from .views import Authorize, Redirect, Capture, AccessTokenView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url('^authorize/?$',
         login_required(Capture.as_view()),
         name='capture'),
@@ -52,4 +53,4 @@ urlpatterns = patterns('',
     url('^access_token/?$',
         csrf_exempt(AccessTokenView.as_view()),
         name='access_token'),
-)
+]
