b9a4427c41436b7faba687527c141e1bc0190e1c
Julien Lirochon <julien@lirochon.net>
2020-09-10 09:10:16 +0200

    Remove from future imports

--- provider/views.py
+++ provider/views.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
-from builtins import object
 import json
 import urllib.parse
 from django.http import HttpResponse
